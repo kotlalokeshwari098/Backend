a6ab121666f66346c55b8ce330f92d1495fb3518
kotlalokeshwari <kotlalokeshwari1871@gmail.com>
2025-05-04 16:21:46 +0530

    created ejs file

--- Dynamic Website with Ejs/server/index.js
+++ Dynamic Website with Ejs/server/index.js
@@ -1,7 +1,13 @@
-const express=reqire('express')
+const express=require('express')
 
 const app=express();
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.set('view engine','ejs')
 
+app.get('/',(req,res)=>{
+    res.render('index');
+})
 
 
 app.listen(5656,()=>{
