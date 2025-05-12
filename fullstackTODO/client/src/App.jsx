e1eb99121a1af687ea2738e0bf2698929b9ed9e9
kotlalokeshwari <kotlalokeshwari1871@gmail.com>
2025-05-12 09:13:48 +0530

    initial setup server

--- fullstackTODO/client/src/App.jsx
+++ fullstackTODO/client/src/App.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
-import ListHeader from './components/ListHeader'
+import React from "react";
+import ListHeader from "./components/ListHeader";
 
 function App() {
+  function signOut(){
+    console.log('signout');
+  }
   return (
-    <div>
-      <ListHeader listName={'holidayss yeahhhhh'}/>
+    <div
+      className="bg-white shadow-md w-[800px] mt-[50px]  
+    flex justify-between"
+    >
+      <ListHeader listName={"Holidays tick list"} />
+      <div className="button-container flex items-center">
+        <button className="create mx-2 my-2 px-5 py-2 rounded-4xl border shadow-gray-950">
+          ADD NEW
+        </button>
+        <button
+          className="signout mx-2 my-2 px-5 py-2 rounded-4xl border shadow-gray-950"
+          onClick={signOut}
+        >
+          SIGN OUT
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- fullstackTODO/client/src/components/ListHeader.jsx
+++ fullstackTODO/client/src/components/ListHeader.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function ListHeader(prop) {
   return (
-    <div className="list-header">
-      <h1>{prop.listName}</h1>
+    <div className="list-header p-[20px] flex justify-between">
+      <h1 className='font-bold text-2xl'>{prop.listName}</h1>
     </div>
   );
 }
